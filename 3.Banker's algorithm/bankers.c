dcb2d633f08d9b6aee19cc5bb72c26e234d73cd1
Maanas MS <109026282+elementaryrock@users.noreply.github.com>
2025-11-01 11:00:18 +0530

    optimised

--- 3.Banker's algorithm/bankers.c
+++ 3.Banker's algorithm/bankers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void main() {
-    int n, m, i, j, avl[30], max[30][30], d = 0, alloc[30][30];
+    int n, m, i, j, avl[30], max[30][30], d = 0, alloc[30][30],c=0;
     
     printf("\n========== BANKER'S ALGORITHM ==========\n");
     
@@ -10,13 +10,6 @@ void main() {
     
     printf("Enter number of resources: ");
     scanf("%d", &m);
-    
-    int T[m];
-    printf("\nEnter total instances of each resource:\n");
-    for (j = 0; j < m; j++) {
-        printf("Resource R%d: ", j);
-        scanf("%d", &T[j]);
-    }
     
     printf("\nEnter the Allocation Matrix:\n");
     for (i = 0; i < n; i++) {
@@ -33,15 +26,13 @@ void main() {
             scanf("%d", &max[i][j]);
         }
     }
-    
+
+    printf("\nEnter the Aval Matrix:\n");
     for (j = 0; j < m; j++) {
-        int c = 0;
-        for (i = 0; i < n; i++)
-            c = c + alloc[i][j];
-        avl[j] = T[j] - c;
+        scanf("%d", &avl[j]);
     }
     
-    int finish[n], safeseq[n], work[n], need[n][m];
+    int finish[n], safeseq[n], work[m], need[n][m];
     
     for (i = 0; i < m; i++)
         work[i] = avl[i];
@@ -58,16 +49,7 @@ void main() {
     printf("SYSTEM STATE ANALYSIS\n");
     printf("========================================\n");
     
-    printf("\nAvailable Resources: ");
-    for (j = 0; j < m; j++)
-        printf("R%d:%d ", j, avl[j]);
-    printf("\n");
-    
     printf("\nNeed Matrix:\n");
-    printf("Process\t");
-    for (j = 0; j < m; j++)
-        printf("R%d\t", j);
-    printf("\n");
     
     for (i = 0; i < n; i++) {
         printf("P%d\t", i);
@@ -91,16 +73,12 @@ void main() {
                     for (int a = 0; a < m; a++)
                         work[a] = work[a] + alloc[i][a];
                     finish[i] = 1;
+                    c++;
                 }
             }
         }
     }
     
-    int c = 0;
-    for (i = 0; i < n; i++) {
-        if (finish[i] == 1)
-            c++;
-    }
     
     printf("\n========================================\n");
     printf("SAFETY ANALYSIS RESULT\n");
